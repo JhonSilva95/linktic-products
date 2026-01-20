6cc248a542cf02b4d443254bcca840302c8c0a20
JhonSilva95 <jhon.sicam95@gmail.com>
2026-01-20 13:41:29 -0500

    Initial commit

---
+++ src/main/java/linktic/products/LinkticProductsApplication.java
@@ -0,0 +1,13 @@
+package linktic.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkticProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkticProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/linktic/products/LinkticProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package linktic.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkticProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
